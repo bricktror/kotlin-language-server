8456fe4014921d767b0a746be2af266e5ebc2d59
Viktor Danhall <bricktror@gmail.com>
2022-10-14 08:44:38 +0200

    Move versions to buildSrc

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,4 +1,8 @@
-
 object Versions {
-    const val kotlin = "1.6.21"
+    const val project = "1.6.10"
+    const val kotlin = "1.6.10" // "1.6.21"?
+    const val exposed = "0.37.3"
+    const val lsp4j = "0.16.0"
+    const val slf4j = "1.6.1"
+    const val java = "19"
 }
--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,18 +1,14 @@
+import Versions
+
 plugins {
     id("bzet.kotlin-application-conventions")
     application
 }
 
-val kotlinVersion: String by extra
-val lsp4jVersion: String by extra
-val exposedVersion: String by extra
-val projectVersion: String by extra
-val slf4jVersion: String by extra
-
 application {
     applicationName="kotlin-language-server"
     mainClass.set("org.javacs.kt.MainKt")
-    applicationDefaultJvmArgs = listOf("-DkotlinLanguageServer.version=$projectVersion")
+    applicationDefaultJvmArgs = listOf("-DkotlinLanguageServer.version=${Versions.project}")
 }
 
 repositories {
@@ -21,18 +17,18 @@ repositories {
 }
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    implementation("org.slf4j:slf4j-simple:$slf4jVersion")
-    implementation("org.eclipse.lsp4j:org.eclipse.lsp4j:$lsp4jVersion")
-    implementation("org.eclipse.lsp4j:org.eclipse.lsp4j.jsonrpc:$lsp4jVersion")
-    implementation("org.jetbrains.kotlin:kotlin-compiler:$kotlinVersion")
-    implementation("org.jetbrains.kotlin:kotlin-scripting-compiler:$kotlinVersion")
-    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm-host-unshaded:$kotlinVersion")
-    implementation("org.jetbrains.kotlin:kotlin-sam-with-receiver-compiler-plugin:$kotlinVersion")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.slf4j:slf4j-api:${Versions.slf4j}")
+    implementation("org.slf4j:slf4j-simple:${Versions.slf4j}")
+    implementation("org.eclipse.lsp4j:org.eclipse.lsp4j:${Versions.lsp4j}")
+    implementation("org.eclipse.lsp4j:org.eclipse.lsp4j.jsonrpc:${Versions.lsp4j}")
+    implementation("org.jetbrains.kotlin:kotlin-compiler:${Versions.kotlin}")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-compiler:${Versions.kotlin}")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm-host-unshaded:${Versions.kotlin}")
+    implementation("org.jetbrains.kotlin:kotlin-sam-with-receiver-compiler-plugin:${Versions.kotlin}")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}")
+    implementation("org.jetbrains.exposed:exposed-core:${Versions.exposed}")
+    implementation("org.jetbrains.exposed:exposed-dao:${Versions.exposed}")
+    implementation("org.jetbrains.exposed:exposed-jdbc:${Versions.exposed}")
 
     implementation("org.jetbrains:fernflower:1.0")
     implementation("com.h2database:h2:1.4.200")
@@ -41,8 +37,9 @@ dependencies {
 
     testImplementation("org.openjdk.jmh:jmh-core:1.20")
 
-    compileOnly("org.jetbrains.kotlin:kotlin-scripting-jvm-host:$kotlinVersion")
-    testCompileOnly("org.jetbrains.kotlin:kotlin-scripting-jvm-host:$kotlinVersion")
+    compileOnly("org.jetbrains.kotlin:kotlin-scripting-jvm-host:${Versions.kotlin}")
+    testCompileOnly("org.jetbrains.kotlin:kotlin-scripting-jvm-host:${Versions.kotlin}")
 
     annotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.20")
 }
+
